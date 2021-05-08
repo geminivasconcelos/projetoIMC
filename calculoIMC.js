f16092cc26ec9e9c2aefbeecd9b6165c98e86e13
Ayo <alvesayo@outlook.com>
2021-05-08 19:06:04 -0300

    adicionei um css

--- calculoIMC.js
+++ calculoIMC.js
@@ -1,8 +1,18 @@
-var nome;
-var idade;
-var peso;
-var altura;
-var calculoIMC;
+var calcular = document.getElementById('calcular');
 
-calculoIMC = peso/(altura*alturo);
+//função para calcular imc
+function imc(){
 
+//pega os valores do input
+    var nome = document.getElementById('nome').value; 
+    var idade = document.getElementById('idade').value;
+    var peso = document.getElementById('peso').value;
+    var altura = document.getElementById('altura').value;
+
+    if(nome !== '' && idade !== '' && peso !== '' && altura !== ''){
+        alert('valor preenchido');
+    }else{
+        alert('valor não preenchido');
+    }
+
+}
